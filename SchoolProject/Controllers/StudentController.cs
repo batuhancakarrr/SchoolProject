a504034713029255467b614f626b835e3045a543
Batuhan Çakar <batuyhan19@gmail.com>
2024-05-08 12:25:20 +0300

    Student detay listesi

--- SchoolProject/Controllers/StudentController.cs
+++ SchoolProject/Controllers/StudentController.cs
@@ -5,10 +5,15 @@ using School.Data.Entities;
 
 namespace SchoolProject.Controllers;
 public class StudentController : Controller {
+	SchoolContext context = new();
 	[Route("Students")]
 	public IActionResult List() {
-		SchoolContext context = new();
 		List<Student> studentList = context.Students.Include(x => x.Class).ThenInclude(x => x.School).ToList();
 		return View(studentList);
+	}
+	[Route("Students/Details/{id}")]
+	public IActionResult Details(int id) {
+		Student student = context.Students.Include(x => x.Class).ThenInclude(x => x.School).FirstOrDefault(x => x.Id == id);
+		return View(student);
 	}
 }
